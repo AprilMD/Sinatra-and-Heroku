0465b426ca73c2052bf2b1f0190a96e863973d7b
April <aprilmdawson@hotmail.com>
2022-07-08 16:08:56 +0100

    add link across pages

--- server.rb
+++ server.rb
@@ -5,16 +5,17 @@ def loading_pictures
     Dir.glob("public/Pictures/*.{jpeg, JPEG}")
 end
 
-# get "/" do
-#     "Hi, visitor, nice to meet you!"
-# end
+get "/" do
+    erb :index
+    # "Hi, visitor, nice to meet you!"
+end
 
 get "/cat" do
     @pictures = loading_pictures
     erb :image
 end
 
-get "/:name?" do |name|
-    erb :index, :locals => {:name=>name}
-    # "Hi, #{name.capitalize}, nice to meet you!"
-end
+# get "/:name?" do |name|
+#     erb :index, :locals => {:name=>name}
+#     # "Hi, #{name.capitalize}, nice to meet you!"
+# end
